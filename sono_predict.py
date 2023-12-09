c0905facb4a82ed4780676f356eeeecdb50929c6
thiagonarcizo <mack094bsb@gmail.com>
2023-12-09 13:12:02 -0300

    Obtaining the dataset from the internet

--- sono_predict.py
+++ sono_predict.py
@@ -5,7 +5,7 @@ import pandas as pd
 import streamlit as st
 
 #ds:
-uri = 'data\ds.csv'
+uri = 'https://raw.githubusercontent.com/thiagonarcizo/Qualidade-do-Sono/main/data/ds.csv'
 ds = pd.read_csv(uri)
 
 #cópia de segurança
@@ -38,24 +38,24 @@ model_sleep_quality = LinearRegression()
 model_sleep_quality.fit(X_train, y_train_sleep_quality)
 
 #faz as predições para duração do sono com base nos inputs pessoais
-age = st.number_input('Insira a sua idade:', min_value=0, max_value=100, value=0, step=1)
-gender = st.selectbox('Selecione seu gênero:', ('Male', 'Female'))
-occupation = st.selectbox('Insira a sua profissão', ('Software Engineer', 'Doctor', 'Sales Representative', 'Teacher', 'Nurse', 'Engineer', 'Accountant', 'Scientist', 'Lawyer', 'Salesperson', 'Manager'))
+idade = st.number_input('Insira a sua idade:', min_value=0, max_value=100, value=0, step=1)
+genero = st.selectbox('Selecione seu gênero:', ('Male', 'Female'))
+prof = st.selectbox('Insira a sua profissão', ('Software Engineer', 'Doctor', 'Sales Representative', 'Teacher', 'Nurse', 'Engineer', 'Accountant', 'Scientist', 'Lawyer', 'Salesperson', 'Manager'))
 stress_level = st.number_input('Seu nível de estresse (0 - 10):', min_value=0, max_value=10, value=0, step=1)
 sleep_disorder = st.selectbox('Em qual dessas condições de sono você melhor se enquadra?', ('None', 'Sleep Apnea', 'Insomnia'))
 
 #cria um dataframe com os dados do usuário
-user_data = pd.DataFrame({'Age': [age], 'Occupation': [occupation], 'Gender': [gender], 'Stress Level': [stress_level], 'Sleep Disorder': ['']})
+user = pd.DataFrame({'Age': [idade], 'Occupation': [prof], 'Gender': [genero], 'Stress Level': [stress_level], 'Sleep Disorder': [sleep_disorder]})
 
 #converte as variáveis categóricas em numéricas (do usuário)
-user_data = pd.get_dummies(user_data)
+user_data = pd.get_dummies(user)
 
 #reindexa o dataframe para que ele tenha as mesmas colunas do X_train
 user_data = user_data.reindex(columns=X_train.columns, fill_value=0)
 
 #faz as predições para duração e qualidade do sono
-sleep_duration_prediction = model_sleep_duration.predict(user_data)
-sleep_quality_prediction = model_sleep_quality.predict(user_data)
+sleep_duration_pred = model_sleep_duration.predict(user_data)
+sleep_quality_pred = model_sleep_quality.predict(user_data)
 
 #calcula o R^2 and MSE para duração e qualidade do sono
 sleep_duration_r2 = r2_score(y_test_sleep_duration, model_sleep_duration.predict(X_test))
@@ -65,7 +65,7 @@ sleep_quality_mse = mean_squared_error(y_test_sleep_quality, model_sleep_quality
 
 #botão para mostrar os resultados:
 if st.button('Mostrar resultados'):
-    st.write('Duração do sono: ', round(sleep_duration_prediction[0], 2), ' horas.')
-    st.write('Qualidade do sono: ', round(sleep_quality_prediction[0], 2), ' (0 - 10).')
+    st.write('Duração do sono: ', round(sleep_duration_pred[0], 2), ' horas.')
+    st.write('Qualidade do sono: ', round(sleep_quality_pred[0], 2), ' (0 - 10).')
     #st.write('Duração do sono: ', sleep_duration_prediction[0], ' horas')
     #st.write('Qualidade do sono: ', sleep_quality_prediction[0], ' (0 - 10)')
